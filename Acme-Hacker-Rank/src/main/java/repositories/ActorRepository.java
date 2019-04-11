b85ce1e1f51db9ab3cb63c32d3231a364ed1cc8e
apcm <apcm@users.noreply.github.com>
2019-04-11 11:14:49 +0200

    Revert "dashboard C"
    This reverts commit 1cb4ccedf42aaac6cf884374bf46f499ef69b12d.

--- Acme-Hacker-Rank/src/main/java/repositories/ActorRepository.java
+++ Acme-Hacker-Rank/src/main/java/repositories/ActorRepository.java
@@ -14,4 +14,6 @@ public interface ActorRepository extends JpaRepository<Actor, Integer> {
 	@Query("select a1 from Actor a1 where a1.userAccount = ?1")
 	public Actor getActor(UserAccount ua);
 
+	@Query("select a from Actor a where a.userAccount.id = ?1")
+	Actor findByUserAccountId(int userAccount);
 }
